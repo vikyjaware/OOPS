03207b11ff1bf2e6c55910b4941a774981147f58
vikyjaware <vikyjaware@gmail.com>
2022-02-09 00:28:14 +0530

    [Viky] Add : Folder structure added

---
+++ InventoryManagement/InventoryManagementMain.cs
@@ -0,0 +1,33 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InventoryManagement
+{
+        public class InventoryManagementMain
+        {
+            public void ReadData(string filepath)
+            {
+                try
+                {
+                    using (StreamReader reader = new StreamReader(filepath))
+                    {
+                        var json = reader.ReadToEnd();
+                        var items = JsonConvert.DeserializeObject<List<Inventory>>(json);
+                        Console.WriteLine("Name\t" + "Price\t" + "Weight\t" + "Total amount");
+                        foreach (var data in items)
+                        {
+                            Console.WriteLine(data.Name + "\t" + data.Price + "\t" + data.Weight + "\t" + data.Price * data.Weight);
+                        }
+                    }
+                }
+                catch (Exception ex)
+                {
+                    throw new Exception(ex.Message);
+                }
+            }
+        }
+    }
---
+++ InventoryManagement/Program.cs
@@ -0,0 +1,6 @@
+﻿// See https://aka.ms/new-console-template for more information
+using InventoryManagement;
+
+InventoryManagementMain inventoryManagement = new InventoryManagementMain();
+inventoryManagement.ReadData(@"D:\bz\Fellowship\OOPS\InventoryManagement\InventoryData.json");
+Console.ReadKey();
